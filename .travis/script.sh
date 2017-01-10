0936416c0d27c51d93592c9da4aab54d24be29ee
Olivier Chédru <ochedru@dhatim.com>
2017-01-10 15:57:55 +0100

    Use parent POM root-oss and simplify release procedure

---
+++ .travis/script.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ "${TRAVIS_EVENT_TYPE}" == push ] &&
+       echo "${TRAVIS_TAG}" | egrep '^[0-9]+\.[0-9]+\.[0-9]+$'
+then
+    # the build is triggered by a tag push, and the tag looks like
+    # a version number: proceed with release
+    echo ${GPG_SECRET_KEY} | base64 --decode | gpg --import
+    echo ${GPG_OWNERTRUST} | base64 --decode | gpg --import-ownertrust
+    mvn versions:set -DnewVersion=${TRAVIS_TAG}
+    mvn -s .travis/settings.xml -Prelease test jacoco:report coveralls:report 
+else
+    # this is a regular build
+    mvn test jacoco:report coveralls:report 
+fi
