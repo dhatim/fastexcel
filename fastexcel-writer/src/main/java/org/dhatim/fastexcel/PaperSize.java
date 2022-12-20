28fb80c5c9dca41143b55c1094f1836c3b6f79a3
TomXin <766781886@qq.com>
2022-12-20 20:20:41 +0800

    Fix javadoc

--- fastexcel-writer/src/main/java/org/dhatim/fastexcel/PaperSize.java
+++ fastexcel-writer/src/main/java/org/dhatim/fastexcel/PaperSize.java
@@ -5,7 +5,6 @@ package org.dhatim.fastexcel;
  * <a href="https://learn.microsoft.com/en-us/previous-versions/office/developer/office-2010/cc880467(v=office.14)?redirectedfrom=MSDN">here</a>.
  *
  * @author apptaro
- * @date 2022/10/11
  */
 public enum PaperSize {
 	LETTER_PAPER(1),
