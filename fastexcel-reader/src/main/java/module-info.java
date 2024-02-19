1a44ecc8497b82a26e4a1c50486540085d59d6e3
Philippe Charles <philippe.charles@nbb.be>
2024-02-19 09:17:27 +0100

    Add full JPMS modules on Java8

---
+++ fastexcel-reader/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module org.dhatim.fastexcel.reader {
+    requires java.xml;
+    requires org.apache.commons.compress;
+    requires com.fasterxml.aalto;
+
+    exports org.dhatim.fastexcel.reader;
+}
